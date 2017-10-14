9d0439c2f44073d83b4d8838b6e0a4bbba11201e
leojacoby <leo_jacoby@branson.org>
2017-10-13 19:05:09 -0700

    fixed double copy error

--- frontend/components/NewEvent.js
+++ frontend/components/NewEvent.js
@@ -18,23 +18,3 @@ NewEvent.propTypes = {
 
 
 export default NewEvent;
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-
-
-const NewEvent = () => {
-    return (
-        <div>
-            <h1>Create a new event!</h1>
-            <Link to="/">Home</Link>
-        </div>
-    );
-};
-
-NewEvent.propTypes = {
-    name: PropTypes.string,
-};
-
-
-export default NewEvent;
